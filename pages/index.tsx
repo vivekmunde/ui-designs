034595e08081dc0f6d01900103d667514c4cf2ac
Vivek <vivekmunde@gmail.com>
2022-07-12 18:46:32 +0530

    play store & app store links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head';
 
+import ExternalLink from '../components/external-link';
 import ImageShowcase from '../components/image-showcase';
 import VideoShowcase from '../components/video-showcase';
 
@@ -20,7 +21,23 @@ const Home: NextPage = () => {
           </h1>
           <div className="mt-1 mb-4 text-center md:text-left">Actual Photos</div>
           <section className="pt-[1px] pb-[1px] mb-12">
-            <h2 className="font-light text-center md:text-left">Admin app</h2>
+            <div className="grid gird-cols-1 lg:grid-cols-2">
+              <h3 className="font-light text-center md:text-left">Zaycare Admin</h3>
+              <div className="flex flex-row justify-center lg:justify-end items-center mb-4 lg:mt-4 lg:mb-0">
+                <ExternalLink href="https://apps.apple.com/sr/app/zaycare/id1558651732">
+                  <img
+                    className="mr-1 bus-store-image"
+                    alt="App Store"
+                    src={`${process.env.NEXT_PUBLIC_ASSET_PREFIX}/images/app-store.png`} />
+                </ExternalLink>
+                <ExternalLink href="https://play.google.com/store/apps/details?id=zay.works.zaycare">
+                  <img
+                    className="bus-store-image"
+                    alt="Play Store"
+                    src={`${process.env.NEXT_PUBLIC_ASSET_PREFIX}/images/play-store.png`} />
+                </ExternalLink>
+              </div>
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 place-items-center">
               <VideoShowcase url="admin.mp4" />
               <ImageShowcase imageAlt="Home" imageUrl="admin-home-1.png" />
@@ -48,8 +65,24 @@ const Home: NextPage = () => {
           </section>
 
           <section className="pt-[1px] pb-[1px] mb-12">
-            <h2 className="font-light text-center md:text-left">Parent app</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+            <div className="grid gird-cols-1 lg:grid-cols-2">
+              <h3 className="font-light text-center md:text-left">Zaycare Parent</h3>
+              <div className="flex flex-row justify-center lg:justify-end items-center mb-4 lg:mt-4 lg:mb-0">
+                <ExternalLink href="https://apps.apple.com/sr/app/zaycare-parent/id1566116225">
+                  <img
+                    className="mr-1 bus-store-image"
+                    alt="App Store"
+                    src={`${process.env.NEXT_PUBLIC_ASSET_PREFIX}/images/app-store.png`} />
+                </ExternalLink>
+                <ExternalLink href="https://play.google.com/store/apps/details?id=com.zaycare.parent">
+                  <img
+                    className="bus-store-image"
+                    alt="Play Store"
+                    src={`${process.env.NEXT_PUBLIC_ASSET_PREFIX}/images/play-store.png`} />
+                </ExternalLink>
+              </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 place-items-center">
               <VideoShowcase url="parent.mp4" />
               <ImageShowcase imageAlt="Home" imageUrl="parent-home-1.png" />
               <ImageShowcase imageAlt="Home" imageUrl="parent-home-2.png" />
